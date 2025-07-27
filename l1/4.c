145453c5609f360814062010951ddc91c01a930e
Ritwik <devilrqop@gmail.com>
2025-07-27 22:28:56 +0530

    wip

--- l1/4.c
+++ l1/4.c
@@ -6,3 +6,38 @@ I - 4 5
 sum  = 7+9i
 mult = 12+20i
 */
+
+#include <stdio.h>
+
+struct Complex {
+    int real;
+    int imag;
+};
+
+
+struct Complex add(struct Complex c1, struct Complex c2) {
+    struct Complex sum;
+    sum.real = c1.real + c2.real;
+    sum.imag = c1.imag + c2.imag;
+    return sum;
+}
+
+
+void multiply(struct Complex *c1, struct Complex *c2, struct Complex *result) {
+    result->real = c1->real * c2->real - c1->imag * c2->imag;
+    result->imag = c1->real * c2->imag + c1->imag * c2->real;
+}
+
+int main() {
+    struct Complex R = {3, 4};  
+    struct Complex I = {4, 5};  
+
+    struct Complex sum = add(R, I);
+    printf("Sum = %d+%di\n", sum.real, sum.imag);
+
+    struct Complex mult;
+    multiply(&R, &I, &mult);
+    printf("Multiplication = %d+%di\n", mult.real, mult.imag);
+
+    return 0;
+}
